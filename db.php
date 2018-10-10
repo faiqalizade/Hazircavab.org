88d1a08df9853c579e1b4bbc06fee18470aa47d9
falizade00 <faiqalizade@MacBook-Air-Faiq.local>
2018-10-11 00:34:38 +0300

    test

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 require 'rb.php';
 R::setup('mysql:host=localhost;dbname=hazircavab2','root','root');
-// R::addDatabase('DB2', 'mysql:host=vh72.timeweb.ru;dbname=faiqaliza3_hazir','faiqaliza3_hazir', 'hazirevtapsiriqlari');
+// R::addDatabase('DB2', 'mysql:host=vh72.timeweb.ru;dbname=faiqaliza3_hazir's,'faiqaliza3_hazir', 'hazirevtapsiriqlari');
 ?>
